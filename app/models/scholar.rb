b0d27bf0beda420f55caaa8b79379cb8653a2239
tech-zack <difference19@icloud.com>
2021-03-10 12:19:32 +0900

    コメント機能

--- app/models/scholar.rb
+++ app/models/scholar.rb
@@ -2,7 +2,7 @@ class Scholar < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :user,   optional: true
-  belongs_to :comment,optional: true
+  belongs_to :message,optional: true
   has_one_attached :image
 
   validates :title, presence: true 
