9a17eefc6c31259c0ea22e6b897d75b749bcc71b
tech-zack <difference19@icloud.com>
2021-03-18 20:09:20 +0900

    フロント実装訂正１

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -8,12 +8,4 @@ environment.plugins.prepend('Provide',
 )
 module.exports = environment
 
-const webpack = require('webpack')
-  environment.plugins.append(
-    'Provide',
-    new webpack.ProvidePlugin({
-      $: 'jquery/src/jquery',
-      jQuery: 'jquery/src/jquery',
-      Popper: ['popper.js', 'default']
-    })
-  )
+
